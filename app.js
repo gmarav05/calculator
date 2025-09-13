03d2c8de848e9614a6a1747c7f61bc1b060859e6
Aravind <gmarav005@gmail.com>
2025-09-13 20:41:41 +0530

    operations are working in js

---
+++ app.js
@@ -0,0 +1,55 @@
+// Your calculator is going to contain functions for all of the basic math operators you typically find on 
+// calculators, so start by creating functions for the following items and testing them in your browser’s
+//  console:
+// add
+// subtract
+// multiply
+// divide
+// A calculator operation will consist of a number, an operator, and another number. 
+// For example, 3 + 5. Create three variables, one for each part of the operation. 
+// You’ll use these variables to update your display later.
+
+
+
+function addition(num1,num2) {
+    const a = parseInt(num1);
+    const b = parseInt(num2);
+    console.log(a+b);
+    // return a+b;
+    // return result;
+}
+
+
+function subtract(num1,num2) {
+    const a = parseInt(num1);
+    const b = parseInt(num2);
+    console.log(a-b);
+}
+
+
+function multiply(num1,num2) {
+     const a = parseInt(num1);
+    const b = parseInt(num2);
+    console.log(a*b);
+}
+
+
+function divide(num1,num2) {
+    const a = parseInt(num1);
+    const b = parseInt(num2);
+    console.log(a/b);
+}
+
+const num1 = window.prompt("Enter number a: ");
+const num2 = window.prompt("Enter number b: ");
+
+// addition(num1,num2);
+
+console.log(  addition(num1,num2));
+
+console.log(subtract(num1,num2));
+
+console.log(multiply(num1,num2));
+
+console.log(divide(num1,num2));
+
